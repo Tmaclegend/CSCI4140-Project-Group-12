a32907f0c01defbef38fad0266a7238101809fd9
Shum Hiu Tung <mike.sam.hell@gmail.com>
2019-05-08 13:24:24 +0800

    static url

--- src/api.js
+++ src/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-const { REACT_APP_REMOTE_SERVER, REACT_APP_REMOTE_SERVER_PORT } = process.env;
+const {
+  REACT_APP_REMOTE_SERVER,
+  REACT_APP_REMOTE_SERVER_PORT
+} = process.env;
 
 export default function getAPI(gameName) {
   return axios.create({
-    baseURL: `${REACT_APP_REMOTE_SERVER}:${Number(REACT_APP_REMOTE_SERVER_PORT) + 1}/games/${gameName}/`
+    // baseURL: `${REACT_APP_REMOTE_SERVER}:${Number(REACT_APP_REMOTE_SERVER_PORT) + 1}/games/${gameName}/`
+
+    baseURL: `http://king-vs-poo-server.herokuapp.com:46740/games/${gameName}/`
   });
 }
